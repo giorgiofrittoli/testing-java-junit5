ed4ad1404f31fce3755783e267b6b0d80e379ae5
frigir <giorgio.frittoli@gmail.com>
2019-08-18 15:37:59 +0200

    disable tests

---
+++ src/test/java/it/frigir/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,50 @@
+package it.frigir.sfgpetclinic.services.springdatajpa;
+
+import it.frigir.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled(value = "Disabled until we learn mocking")
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService ownerSDJpaService;
+
+    @BeforeEach
+    void setUp() {
+        ownerSDJpaService = new OwnerSDJpaService(null, null, null);
+    }
+
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner owner = ownerSDJpaService.findByLastName("buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
